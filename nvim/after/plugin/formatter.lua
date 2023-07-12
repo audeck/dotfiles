4f3b33e848932a7ba10755eb9e313b30c1e25974
audeck <audeck@duck.com>
2023-07-13 00:06:55 +0200

    Added formatter, changes

---
+++ nvim/after/plugin/formatter.lua
@@ -0,0 +1,24 @@
+require("formatter").setup({
+	logging = true,
+	log_level = vim.log.levels.WARN,
+	filetype = {
+		lua = require("formatter.filetypes.lua").stylua,
+		cs = require("formatter.filetypes.cs").dotnetformat,
+		css = require("formatter.filetypes.css").stylefmt,
+		html = require("formatter.filetypes.html").htmlbeautify,
+		-- markdown = require("formatter.filetypes.markdown").prettier,
+		rust = require("formatter.filetypes.rust").rustfmt,
+		typescript = require("formatter.filetypes.typescript").eslint_d,
+		typescriptreact = require("formatter.filetypes.typescriptreact").eslint_d,
+	},
+})
+
+vim.api.nvim_exec(
+	[[
+        augroup FormatAutogroup
+            autocmd!
+            autocmd BufWritePost * FormatWrite
+        augroup END
+    ]],
+	false
+)
